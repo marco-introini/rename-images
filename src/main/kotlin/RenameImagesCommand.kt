53591afb834499cf3700d05651440a27cc4aa63b
Marco "Mint" Introini <mint.dev@pm.me>
2024-09-24 11:27:41 +0200

    Refactor ImageCollection initialization in RenameImagesCommand
    Moved ImageCollection initialization logic to a companion object function fromDirectory. This change simplifies the directory checking and file listing process in RenameImagesCommand, making the code more modular and readable.

--- src/main/kotlin/RenameImagesCommand.kt
+++ src/main/kotlin/RenameImagesCommand.kt
@@ -5,33 +5,20 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import me.mintdev.models.ImageCollection
 import java.io.File
 
-class RenameImagesCommand: CliktCommand() {
+class RenameImagesCommand : CliktCommand() {
     val directory: String by argument()
 
     override fun run() {
-        val imagesCollection = ImageCollection()
         val dir = File(directory)
-
+        val imageCollection: ImageCollection
         // Check if the directory exists and if it is a directory (not a file)
         if (dir.exists() && dir.isDirectory) {
             println("Existing directory: $directory")
-
-            // Get the list of all files in the directory
-            val files = dir.listFiles()
-
-            // Check if there are files in the directory
-            if (files != null && files.isNotEmpty()) {
-                files.forEach { file ->
-                    imagesCollection.add(file.name)
-                }
-                println("Found ${imagesCollection.images.size} files.")
-                println(imagesCollection)
-
-            } else {
-                println("The directory is empty or does not contain files.")
-            }
+            imageCollection = ImageCollection.fromDirectory(directory)
+            println("Found ${imageCollection.images.size} files.")
+            println(imageCollection)
         } else {
-            println("Error: $directory is not an existing directory.")
+            println("The directory is empty or does not contain files.")
         }
     }
 }
--- src/main/kotlin/models/ImageCollection.kt
+++ src/main/kotlin/models/ImageCollection.kt
@@ -1,7 +1,29 @@
 package me.mintdev.models
 
-class ImageCollection {
+import kotlin.collections.forEach
+import kotlin.collections.isNotEmpty
+import java.io.File
+
+class ImageCollection() {
     val images = mutableListOf<String>()
+
+    companion object {
+        fun fromDirectory(directory: String): ImageCollection {
+            val dir = File(directory)
+            val imagesCollection = ImageCollection()
+            // Get the list of all files in the directory
+            val files = dir.listFiles()
+
+            // Check if there are files in the directory
+            if (files != null && files.isNotEmpty()) {
+                files.forEach { file ->
+                    imagesCollection.add(file.name)
+                }
+                return imagesCollection
+            }
+            throw Exception("Directory not found or is empty.")
+        }
+    }
 
     fun add(image: String) {
         if (isValidImage(image)) {
